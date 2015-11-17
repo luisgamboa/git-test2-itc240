265d7ff73c202ab5875c9ccf44daeb6008cb75c3
Luis Gamboa <luisgamboasierra@gmail.com>
2015-11-16 16:52:04 -0800

    Initial test page made
    Here is the first edit in this file

---
+++ test.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+    <h1>Imagine this is a cool webpage</h1>
+    <p>Version 2</p>
+    <p>CLever content goes here</p>
+    <p>CLever content goes here</p>
+    <p>CLever content goes here</p>
+    <p>CLever content goes here</p>
+
+</body>
+</html>
