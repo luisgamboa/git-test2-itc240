cad697f58acfc0654940d92712b060aff4fb274e
Luis Gamboa <luisgamboasierra@gmail.com>
2015-11-16 16:59:28 -0800

    This is my second change to index.php

--- test.php
+++ test.php
@@ -3,11 +3,13 @@
 </head>
 <body>
     <h1>Imagine this is a cool webpage</h1>
-    <p>Version 2</p>
+    
+    <p>Version 3</p>
+    
+    <p>Different CLever content goes here</p>
     <p>CLever content goes here</p>
-    <p>CLever content goes here</p>
-    <p>CLever content goes here</p>
-    <p>CLever content goes here</p>
+    <p>This is also CLever content different that goes here</p>
+
 
 </body>
 </html>
